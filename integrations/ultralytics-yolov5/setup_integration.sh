85c79666443ad13d74fa1219fb235c140d6326ca
Erkka Tahvanainen <59445256+tahvane1@users.noreply.github.com>
2022-05-26 16:17:28 +0300

    Update setup_integration.sh

--- integrations/ultralytics-yolov5/setup_integration.sh
+++ integrations/ultralytics-yolov5/setup_integration.sh
@@ -4,7 +4,7 @@
 # Creates a yolov5 folder next to this script with all required dependencies from the ultralytics/yolov5 repository.
 # Command: `bash setup_integration.sh`
 
-git clone https://github.com/neuralmagic/yolov5.git
+git clone git clone https://ErkkaT@bitbucket.org/ErkkaT/yolov5.git
 cd yolov5
 git checkout release/0.12
 pip install -r requirements.txt
